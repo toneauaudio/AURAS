348bec8ab8bf5cbe95b041628100cf9f1745c961
mcollins21 <mcollins21@luc.edu>
2024-01-23 23:05:52 -0500

    added function declarations

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*//////////////////////////////////////////////////////////////
+// AURAS
+//
+// AURAS is a prototype acoustic vehicle alerting system (AVAS) for both hybrid and electric vehicles.  
+//
+// © Toneau Audio 2024
+*///////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//Function Declarations
+///////////////////////
+
+
+
+int main() {
+
+    
+
+}
